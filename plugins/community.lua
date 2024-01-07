640eb8a7cd509d9c9abaca32c9abd085b2971748
Aditya Dhara <dev@adityadhara.com>
2024-01-06 22:36:04 -0800

    Updated plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -8,4 +8,27 @@ return {
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.python" },
+
+  -- documentation
+  { import = "astrocommunity.pack.markdown" },
+
+  -- config/data files
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.toml" },
+
+  -- web dev
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.tailwindcss" },
+
+  -- misc
+  { import = "astrocommunity.pack.quarto" },
+
+  -- editor enhancements
+  { import = "astrocommunity.editing-support.auto-save-nvim" },
+  { import = "astrocommunity.editing-support.multicursors-nvim" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
 }
